359d740056b982311fe6f7b36cdea3692d0e1130
Jon Clayden <code@clayden.org>
2017-03-27 11:45:23 +0100

    Adding formula

---
+++ Formula/tractor.rb
@@ -0,0 +1,35 @@
+class Tractor < Formula
+  desc "TractoR: Magnetic resonance and tractography with R"
+  homepage "http://www.tractor-mri.org.uk"
+  url "http://www.tractor-mri.org.uk/tractor.tar.gz"
+  version "3.0.8"
+  sha256 "e4d647dd09bd9a5a7ec3a298996d18d1f41fada14be4bae8c489f0c6e839b2cf"
+  head "https://github.com/tractor/tractor.git"
+
+  depends_on "homebrew/science/r"
+
+  def install
+    ENV.deparallelize
+
+    system "make", "install"
+    
+    prefix.install Dir["*.md", "VERSION", "tests"]
+    
+    bin.install "bin/tractor", "bin/plough"
+    libexec.install "bin/exec/tractor", "lib/R"
+    share.install "share/man", "share/tractor"
+    doc.install Dir["share/doc/*"]
+    
+    prefix.install_symlink libexec => "lib"
+    (bin/"exec").mkpath
+    (bin/"exec").install_symlink libexec/"tractor"
+  end
+
+  test do
+    system "#{bin}/tractor", "platform"
+  end
+  
+  def caveats
+    "You should probably set the TRACTOR_HOME environment variable to #{opt_prefix}"
+  end
+end
