b2b1d972891cb5a3280416414dce66fa2cac40f0
Jon Clayden <code@clayden.org>
2017-09-27 23:05:04 +0100

    Release 3.1.3

--- Formula/tractor.rb
+++ Formula/tractor.rb
@@ -2,8 +2,8 @@ class Tractor < Formula
   desc "TractoR: Magnetic resonance and tractography with R"
   homepage "http://www.tractor-mri.org.uk"
   url "http://www.tractor-mri.org.uk/tractor.tar.gz"
-  version "3.1.2"
-  sha256 "6bd7503637ce0a168462f0523860ec1364180d701e75b73c150c31789768aeb6"
+  version "3.1.3"
+  sha256 "b3d74ee72e6f1ddbdef153c493a89e563a13fa5bb52493e161dddbf2f451fd9d"
   head "https://github.com/tractor/tractor.git"
 
   depends_on "r"
