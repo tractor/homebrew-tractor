3f8460571d05ddad8854c8b32aba6251bfd0fd53
Jon Clayden <code@clayden.org>
2021-01-12 21:53:19 +0000

    Release 3.3.4

--- Formula/tractor.rb
+++ Formula/tractor.rb
@@ -2,18 +2,12 @@ class Tractor < Formula
   desc "TractoR: Magnetic resonance and tractography with R"
   homepage "http://www.tractor-mri.org.uk"
   url "http://www.tractor-mri.org.uk/tractor.tar.gz"
-  version "3.3.3"
-  sha256 "d78ae0afa0ec36162c011095a37ca61c812d7c039719c492b637e7be5d5f9275"
+  version "3.3.4"
+  sha256 "01eb640b36493d72b1824cb826bc7dc83d55585bf2d17c36f57c9803c724fc8e"
   head "https://github.com/tractor/tractor.git"
   
   depends_on "r"
   depends_on "jasper"   # for JPEG-2000 support in divest
-  
-  bottle do
-    root_url "https://www.tractor-mri.org.uk"
-    cellar :any
-    sha256 "eae0a368a6cbdfd78c9e2ef844d1bce768ec8f7aa53002abd3d2b7a62c4a5841" => :catalina
-  end
   
   def install
     ENV.deparallelize
