60a84d8f5340968d8eff0f95f1f1892061fdb5e9
Jon Clayden <code@clayden.org>
2021-04-29 15:29:46 +0100

    Updating formula style

--- Formula/tractor.rb
+++ Formula/tractor.rb
@@ -1,44 +1,43 @@
 class Tractor < Formula
-  desc "TractoR: Magnetic resonance and tractography with R"
+  desc "Magnetic resonance and tractography with R"
   homepage "http://www.tractor-mri.org.uk"
   url "http://www.tractor-mri.org.uk/tractor.tar.gz"
   version "3.3.4"
   sha256 "01eb640b36493d72b1824cb826bc7dc83d55585bf2d17c36f57c9803c724fc8e"
   head "https://github.com/tractor/tractor.git"
-  
-  depends_on "r"
-  depends_on "jasper"   # for JPEG-2000 support in divest
-  
+
   bottle do
     root_url "https://www.tractor-mri.org.uk"
-    cellar :any
-    sha256 "2d42a38ff200055ae0e7698fd7c8f6ccf03d0e7758a5156739bfa2d144e2beb0" => :catalina
-    sha256 "fdcac2ab25146ee1580a491aa8934e9d3cc79eb304a1a32d7b92f4d06d8b7911" => :big_sur
-    sha256 "f23217b7a21d37351888a0f7ec2fca4719d8a48fb5ba97715829a1c678761230" => :arm64_big_sur
+    sha256 cellar: :any, arm64_big_sur: "f23217b7a21d37351888a0f7ec2fca4719d8a48fb5ba97715829a1c678761230"
+    sha256 cellar: :any, big_sur:       "fdcac2ab25146ee1580a491aa8934e9d3cc79eb304a1a32d7b92f4d06d8b7911"
+    sha256 cellar: :any, catalina:      "2d42a38ff200055ae0e7698fd7c8f6ccf03d0e7758a5156739bfa2d144e2beb0"
   end
-  
+
+  depends_on "jasper"    # for JPEG-2000 support in divest
+  depends_on "r"
+
   def install
     ENV.deparallelize
-    
+
     system "make", "install"
-    
+
     prefix.install Dir["*.md", "VERSION", "tests"]
-    
+
     bin.install "bin/tractor", "bin/plough", "bin/furrow"
     libexec.install "bin/exec/tractor", "lib/R"
     share.install "share/man", "share/tractor"
     doc.install Dir["share/doc/*"]
-    
+
     prefix.install_symlink libexec => "lib"
     (bin/"exec").mkpath
     (bin/"exec").install_symlink libexec/"tractor"
   end
-  
-  test do
-    system "#{bin}/tractor", "platform"
-  end
-  
+
   def caveats
     "You should probably set the TRACTOR_HOME environment variable to #{opt_prefix}"
+  end
+
+  test do
+    system "#{bin}/tractor", "platform"
   end
 end
