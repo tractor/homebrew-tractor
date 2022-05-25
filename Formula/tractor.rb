74fc7bf308304709b5ad2ea344eee87cc797011d
Jon Clayden <code@clayden.org>
2022-05-25 12:31:32 +0100

    Creating revision to prompt bottle rebuild against R 4.2.0

--- Formula/tractor.rb
+++ Formula/tractor.rb
@@ -5,14 +5,7 @@ class Tractor < Formula
   version "3.3.5"
   sha256 "b4ae366b006c64415c3149d1d06c1158a914a4245c1e049ed036c064e2712e3b"
   head "https://github.com/tractor/tractor.git"
-
-  bottle do
-    root_url "https://www.tractor-mri.org.uk"
-    rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "fe91dbd20784dfad4f0703ec1176e08ad5c11d2588a564e0ceae152d9f2ab06a"
-    sha256 cellar: :any, big_sur:       "c289cda12576e2750863f750a3fd44d1b780bf9f08dfce32a3f73d8d5c8f5b87"
-    sha256 cellar: :any, catalina:      "f0dd461d6ddd45118e8f968e62658fc9c9d0ab1d24c7b0b699545d5a56eedaf6"
-  end
+  revision 1
 
   depends_on "jasper"    # for JPEG-2000 support in divest
   depends_on "r"
