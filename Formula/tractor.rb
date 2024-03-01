1bbe0ffa753326699d43fb9ab2a476b7bc058a0b
Jon Clayden <code@clayden.org>
2024-03-01 14:44:26 +0000

    Release 3.4.2

--- Formula/tractor.rb
+++ Formula/tractor.rb
@@ -2,16 +2,9 @@ class Tractor < Formula
   desc "Magnetic resonance and tractography with R"
   homepage "http://www.tractor-mri.org.uk"
   url "http://www.tractor-mri.org.uk/tractor.tar.gz"
-  version "3.4.1"
-  sha256 "4aa00b8afdee259e881e07602cb9f9c47a979cc99fb6b2fb521ceafd0831fa10"
+  version "3.4.2"
+  sha256 "982b75d27b4aef00c9d8804cc2c90b9bafdce0d816150141ff232db746755c9f"
   head "https://github.com/tractor/tractor.git"
-  
-  bottle do
-    root_url "https://www.tractor-mri.org.uk"
-    rebuild 1
-    sha256 cellar: :any, ventura: "b62f9a2a026c48c957292a3c56a4f7c8e60cb50f592885f7cb4bcec94b8be55c"
-    sha256 cellar: :any, arm64_ventura: "b75d4df729e626267e27eb75b9e9319ef53d667f90edf97dbb34bf2b3fc3fabc"
-  end
   
   depends_on "jasper"    # for JPEG-2000 support in divest
   depends_on "r"
